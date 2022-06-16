7ccc6958c845f30e592699876b9ae601153d2fe8
tan <tanmayrajkrishna@gmail.com>
2022-06-16 11:26:39 +0530

    finally fix scroll

--- scripts/post.js
+++ scripts/post.js
@@ -14,7 +14,7 @@ document.body.addEventListener("touchmove", onScroll)
 function onScroll() {
     const { scrollTop, scrollHeight, clientHeight} = document.documentElement;
     if (scrollTop + clientHeight >= scrollHeight - 60) {
-        loadPosts(10)
+        loadPosts(5)
     }
 }
 
