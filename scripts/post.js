6ec70ab919e032699d3999a37612fda2b94c54e0
tan <tanmayrajkrishna@gmail.com>
2022-06-17 17:24:43 +0530

    add caching

--- scripts/post.js
+++ scripts/post.js
@@ -1,5 +1,6 @@
 let posts
 let currentIndex = 0
+const cache = 'caches' in self ? await caches.open("items") : undefined
 
 export function init(info) {
     getPosts(info["page"]).then(data => {
@@ -23,63 +24,75 @@ async function getPosts(page) {
     return res.json()
 }
 
-async function createPost(id) {
-    let res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-    res.json()
-    .then(data => {
-        
-        if (data["type"] === "poll") return
-
-        const main = document.getElementsByTagName("main")
-
-        const postDiv = document.createElement("div")
-        postDiv.className = "post"
-        main[0].appendChild(postDiv)
-
-        const postFlexDiv = document.createElement("div")
-        postFlexDiv.className = "postflex"
-        postDiv.appendChild(postFlexDiv)
-
-        const postBottomDiv = document.createElement("div")
-        postBottomDiv.className = "postbottom"
-        postDiv.appendChild(postBottomDiv)
-
-        const postIdentifierDiv = document.createElement("div")
-        postIdentifierDiv.className = "postidentifier"
-        postFlexDiv.appendChild(postIdentifierDiv)
-
-
-        const postBodyDiv = document.createElement("div")
-        postBodyDiv.className = "postbody"
-        postFlexDiv.appendChild(postBodyDiv)
-
-        const postHeadDiv = document.createElement("h4")
-        postHeadDiv.className = "posthead"
-        if (data["url"] != undefined) {
-            postHeadDiv.innerHTML = `<a href="${data["url"]}">${data["title"]}</a>`
-        } else {
-            postHeadDiv.innerHTML = `<a href="https://news.ycombinator.com/item?id=${id}">${data["title"]}</a>`
+async function getItemInfo(id) {
+    let url = `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+    if (!cache) {
+        return await fetch(url).then(data => data.json)
+    } else {
+        if (!(await cache.match(url))) {
+            await cache.add(url)
         }
-        postBodyDiv.appendChild(postHeadDiv)
+        return await cache.match(url).then(data => data.json())
+    }
+    
 
-        if (data["text"] != undefined) {
-            const postTextDiv = document.createElement("p")
-            postTextDiv.className = "posttext"
-            postTextDiv.innerHTML = data["text"]
-            postBodyDiv.appendChild(postTextDiv)
-        }
+}
 
-        const postBottomInfo = document.createElement("p")
-        postBottomInfo.innerHTML = `${data["score"]} points | by <a href="https://news.ycombinator.com/user?id=${data["by"]}">${data["by"]}</a> | <a href="https://news.ycombinator.com/item?id=${id}">discuss</a>`
-        postBottomDiv.appendChild(postBottomInfo)
-    })
+function createPost(id, title, url, type, by, text, score) {
+
+    if (type === "poll") return
+
+    const main = document.getElementsByTagName("main")
+
+    const postDiv = document.createElement("div")
+    postDiv.className = "post"
+    main[0].appendChild(postDiv)
+
+    const postFlexDiv = document.createElement("div")
+    postFlexDiv.className = "postflex"
+    postDiv.appendChild(postFlexDiv)
+
+    const postBottomDiv = document.createElement("div")
+    postBottomDiv.className = "postbottom"
+    postDiv.appendChild(postBottomDiv)
+
+    const postIdentifierDiv = document.createElement("div")
+    postIdentifierDiv.className = "postidentifier"
+    postFlexDiv.appendChild(postIdentifierDiv)
+
+
+    const postBodyDiv = document.createElement("div")
+    postBodyDiv.className = "postbody"
+    postFlexDiv.appendChild(postBodyDiv)
+
+    const postHeadDiv = document.createElement("h4")
+    postHeadDiv.className = "posthead"
+    if (url != undefined) {
+        postHeadDiv.innerHTML = `<a href="${url}">${title}</a>`
+    } else {
+        postHeadDiv.innerHTML = `<a href="https://news.ycombinator.com/item?id=${id}">${title}</a>`
+    }
+    postBodyDiv.appendChild(postHeadDiv)
+
+    if (text != undefined) {
+        const postTextDiv = document.createElement("p")
+        postTextDiv.className = "posttext"
+        postTextDiv.innerHTML = text
+        postBodyDiv.appendChild(postTextDiv)
+    }
+
+    const postBottomInfo = document.createElement("p")
+    postBottomInfo.innerHTML = `${score} points | by <a href="https://news.ycombinator.com/user?id=${by}">${by}</a> | <a href="https://news.ycombinator.com/item?id=${id}">discuss</a>`
+    postBottomDiv.appendChild(postBottomInfo)
 }
 
 async function loadPosts(count) {
     for (let i = 0; i < count; i++) {
         if (posts[currentIndex] != null) {
             currentIndex += 1
-            await createPost(posts[currentIndex - 1])            
+            await getItemInfo(posts[currentIndex - 1]).then(data => {
+                createPost(data["id"], data["title"], data["url"], data["type"], data["by"], data["text"], data["score"])
+            })
         } else break
     }
 }
